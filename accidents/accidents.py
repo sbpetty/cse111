364bfe7f68f392d7c724b7c2a4b6ccf82fedc945
Benjamin Petty <pet23024@byui.edu>
2026-06-24 13:00:03 -0600

    Added code to handle FileNotFoundError

--- accidents/accidents.py
+++ accidents/accidents.py
@@ -18,41 +18,46 @@ FATIGUE_COLUMN = 9
 
 def main():
     # Prompt the user for a filename and open that text file.
-    filename = input("Name of file that contains NHTSA data: ")
-    with open(filename, "rt") as text_file:
+    try:
+        filename = input("Name of file that contains NHTSA data: ")
+    
+        with open(filename, "rt") as text_file:
 
-        # Prompt the user for a percentage.
-        perc_reduc = float(input(
-            "Percent reduction of texting while driving [0, 100]: "))
+            # Prompt the user for a percentage.
+            perc_reduc = float(input(
+                "Percent reduction of texting while driving [0, 100]: "))
 
-        print()
-        print(f"With a {perc_reduc}% reduction in using a cell",
-            "phone while driving, approximately the",
-            "following number of injuries and deaths",
-            "would have been prevented in the USA.", sep="\n")
-        print()
-        print("Year, Injuries, Deaths")
+            print()
+            print(f"With a {perc_reduc}% reduction in using a cell",
+                "phone while driving, approximately the",
+                "following number of injuries and deaths",
+                "would have been prevented in the USA.", sep="\n")
+            print()
+            print("Year, Injuries, Deaths")
 
-        # Use the csv module to create a reader
-        # object that will read from the opened file.
-        reader = csv.reader(text_file, strict=True)
+            # Use the csv module to create a reader
+            # object that will read from the opened file.
+            reader = csv.reader(text_file, strict=True)
 
-        # The first line of the CSV file contains column
-        # headings and not data about accidents so this
-        # statement skips the first line of the CSV file.
-        next(reader)
+            # The first line of the CSV file contains column
+            # headings and not data about accidents so this
+            # statement skips the first line of the CSV file.
+            next(reader)
 
-        # Process each row in the CSV file.
-        for row in reader:
-            year = row[YEAR_COLUMN]
+            # Process each row in the CSV file.
+            for row in reader:
+                year = row[YEAR_COLUMN]
 
-            # Call the estimate_reduction function.
-            injur, fatal = estimate_reduction(
-                    row, PHONE_COLUMN, perc_reduc)
+                # Call the estimate_reduction function.
+                injur, fatal = estimate_reduction(
+                        row, PHONE_COLUMN, perc_reduc)
 
-            # Print the estimated reductions
-            # in injuries and fatalities.
-            print(year, injur, fatal, sep=", ")
+                # Print the estimated reductions
+                # in injuries and fatalities.
+                print(year, injur, fatal, sep=", ")
+                
+    except FileNotFoundError:
+        print(f"Error: {filename} does not exist")
 
 
 def estimate_reduction(row, behavior_key, perc_reduc):
